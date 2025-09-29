201419335ae99b4aaf2b1efe86c3fe9eb25a7452
AdelleRo <adelinedewet424@gmail.com>
2025-09-29 14:59:59 +0200

    added comment about config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+/**necessary if you want to customize Tailwind CSS settings, specify which files Tailwind should scan for class names, or add plugins.*/
+/**ensures Tailwind only includes styles actually used in your HTML and JS files, making your CSS smaller and more efficient. */
+
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
